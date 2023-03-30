fd4ef4442ffb3ffb48e3cd18ea474b098787c97c
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2023-03-30 18:57:36 +0300

    create UseController, replace controllers from index.js

---
+++ controllers/UserController.js
@@ -0,0 +1,119 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { validationResult } from "express-validator";
+import UserModel from "../models/User.js";
+
+//авторизация
+export const login = async (req, res) => {
+  try {
+    // проверяем есть ли емайл в базе
+    const user = await UserModel.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(404).json({
+        message: "Пользователь не найден",
+      });
+    }
+
+    const isValidPass = await bcrypt.compare(
+      req.body.password,
+      user._doc.passwordHash
+    );
+
+    if (!isValidPass) {
+      return res.status(400).json({
+        message: "Неверный логин или пароль",
+      });
+    }
+    // если все норм, то создаем токен
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret123",
+      {
+        expiresIn: "30d", //срок жизни 30 дней
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Не удалось авторизоваться" });
+  }
+};
+
+//регистрация
+export const register = async (req, res) => {
+  try {
+    console.log(req.body);
+    //проверка на ошибки
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    // шифруем пароль
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: hash,
+    });
+
+    // создаем пользователя в монгоДБ
+    const user = await doc.save();
+
+    // создаем токен jwt
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret123",
+      {
+        expiresIn: "30d", //срок жизни 30 дней
+      }
+    );
+
+    // возвращаем информацию о пользователе и токен
+    // passwordHash отделяем от др. свойств, чтобы не возвращать его
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Не удалось зарегистрироваться" });
+  }
+};
+
+// информация обо мне
+export const getMe = async (req, res) => {
+  try {
+    // если пользователь есть найди его по ID
+    const user = await UserModel.findById(req.userId);
+    if (!user) {
+      return res.status(404).json({
+        message: "Пользователь не найден",
+      });
+    }
+
+    const { passwordHash, ...userData } = user._doc;
+    res.json(userData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Нет доступа",
+    });
+  }
+};
