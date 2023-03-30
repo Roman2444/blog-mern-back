c26cb49015c3655c7025103b7399bc23cc8d498a
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2023-03-30 18:12:04 +0300

    add func chekAuth, and app.get about me

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import { validationResult } from "express-validator";
 import { registerValidation } from "./validations/auth.js";
 
 import UserModel from "./models/User.js";
+import checkAuth from "./utils/checkAuth.js";
 
 mongoose
   .connect(
@@ -109,6 +110,27 @@ app.post("/auth/register", registerValidation, async (req, res) => {
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Не удалось зарегистрироваться" });
+  }
+});
+
+// страници обо мне
+app.get("/auth/me", checkAuth, async (req, res) => {
+  try {
+    // если пользователь есть найди его по ID
+    const user = await UserModel.findById(req.userId);
+    if (!user) {
+      return res.status(404).json({
+        message: "Пользователь не найден",
+      });
+    }
+
+    const { passwordHash, ...userData } = user._doc;
+    res.json(userData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Нет доступа",
+    });
   }
 });
 
---
+++ utils/checkAuth.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+
+export default (req, res, next) => {
+  // необходимо спарсить токен и расшифровать
+  // проверяем есть токен или нет, удаляем "Bearer" в начале токена
+  const token = (req.headers.authorization || "").replace(/Bearer\s?/, "");
+  if (token) {
+    try {
+        const decoded = jwt.verify(token, 'secret123')
+
+        req.userId = decoded._id
+        next()
+    } catch (err) {
+        return res.status(403).json({
+            message: "Нет доступа",
+          });
+    }
+  } else {
+    return res.status(403).json({
+      message: "Нет доступа",
+    });
+  }
+};
