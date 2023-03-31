8cc8f00130670cf5b454949f532fe5db08aad9eb
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2023-03-31 11:30:27 +0300

    add create in postController

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,7 +1,20 @@
-export const create = (req, res) => {
-    try {
+import PostModel from "../models/Post";
 
-    } catch (err) {
-        
-    }
-}
+export const create = async (req, res) => {
+  try {
+    const doc = new PostModel({
+      title: req.body.title,
+      text: req.body.text,
+      imageUrl: req.body.imageUrl,
+      tags: req.body.tags.split(","),
+      user: req.userId,
+    });
+
+    const post = await doc.save();
+
+    res.json(post)
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Не удалось создать статью" });
+  }
+};
