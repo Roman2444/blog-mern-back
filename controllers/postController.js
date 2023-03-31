559126fafe476c212be6f6b036290f3aa09481c1
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2023-03-31 14:20:59 +0300

    add getOne in PostController

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,18 +1,66 @@
-import PostModel from "../models/Post";
+import PostModel from "../models/Post.js";
 
+// получаем все статьи
+export const getAll = async (req, res) => {
+  try {
+    // populate("user").exec() чтобы получить данные о привязанном пользователе статьи
+    const posts = await PostModel.find().populate("user").exec();
+    res.json(posts);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Не удалось получить статьи" });
+  }
+};
+
+// получаем 1 статью
+export const getOne = async (req, res) => {
+  try {
+    const postId = req.params.id; // вытаскиваем ID статьи которую будем искать
+
+    // нам нужно найти статью и увеличить количество просмотров на 1
+    PostModel.findOneAndUpdate(
+      {
+        _id: postId,
+      },
+      {
+        $inc: { viewsCount: 1 }, // передаем что будем изменять на 1
+      },
+      {
+        returnDocument: "after", // после обновления вернем актуальный документ
+      },
+      (err, doc) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({ message: "Не удалось вернуть статью" });
+        }
+        if (!doc) {
+          return res.status(404).json({
+            message: "Статья не найдена",
+          });
+        }
+        res.json(doc);
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Не удалось получить статью" });
+  }
+};
+
+// создаем статью
 export const create = async (req, res) => {
   try {
     const doc = new PostModel({
       title: req.body.title,
       text: req.body.text,
       imageUrl: req.body.imageUrl,
-      tags: req.body.tags.split(","),
+      tags: req.body.tags,
       user: req.userId,
     });
 
     const post = await doc.save();
 
-    res.json(post)
+    res.json(post);
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Не удалось создать статью" });
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
-import { registerValidation, loginValidation } from "./validations.js";
+import {
+  registerValidation,
+  loginValidation,
+  postCreateValidation,
+} from "./validations.js";
 import checkAuth from "./utils/checkAuth.js";
+import * as PostController from "./controllers/PostController.js";
 import * as UserController from "./controllers/UserController.js";
 
 mongoose
@@ -16,12 +21,17 @@ app.use(express.json());
 
 //авторизация
 app.post("/auth/login", loginValidation, UserController.login);
-
 // страница регистрации
 app.post("/auth/register", registerValidation, UserController.register);
-
 // страници обо мне
 app.get("/auth/me", checkAuth, UserController.getMe);
+
+// работа с постами
+app.get("/posts", PostController.getAll);
+app.get("/posts/:id", PostController.getOne);
+app.post("/posts", checkAuth, postCreateValidation, PostController.create);
+// app.delete("/posts",  PostController.remove);
+// app.patch("/posts",  PostController.update);
 
 app.listen(4444, (err) => {
   if (err) {
