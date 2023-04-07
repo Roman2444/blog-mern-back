1896ace2006c6d04fd05a0638f2b5987bb1025af
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2023-04-07 15:57:03 +0300

    corrected checkAuth from authorization to authorisation

--- utils/checkAuth.js
+++ utils/checkAuth.js
@@ -3,17 +3,17 @@ import jwt from "jsonwebtoken";
 export default (req, res, next) => {
   // необходимо спарсить токен и расшифровать
   // проверяем есть токен или нет, удаляем "Bearer" в начале токена
-  const token = (req.headers.authorization || "").replace(/Bearer\s?/, "");
+  const token = (req.headers.authorisation || "").replace(/Bearer\s?/, "");
   if (token) {
     try {
-        const decoded = jwt.verify(token, 'secret123')
+      const decoded = jwt.verify(token, "secret123");
 
-        req.userId = decoded._id
-        next()
+      req.userId = decoded._id;
+      next();
     } catch (err) {
-        return res.status(403).json({
-            message: "Нет доступа",
-          });
+      return res.status(403).json({
+        message: "Нет доступа",
+      });
     }
   } else {
     return res.status(403).json({
